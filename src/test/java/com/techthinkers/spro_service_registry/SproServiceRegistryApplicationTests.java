cbe8b76f3b96a0f9ce1640e1b1a5d34c9b0fc667
Owais Khan <owais.nitk@gmail.com>
2024-07-29 11:03:24 +0530

    initial commit and setting up project

---
+++ src/test/java/com/techthinkers/spro_service_registry/SproServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techthinkers.spro_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SproServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
